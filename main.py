156df705c057f2209bdeb7fdb6bc4043012200ea
Manoj <penugurthimanoj@gmail.com>
2024-01-17 17:23:43 +0530

    added 3 more commands

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import webbrowser
 import sys
+import colorama
+from colorama import Fore
+
+colorama.init(autoreset = True)
 
 site = sys.argv[1]
 
 if site == "sp":
-  print("Searching Spotify...")
+  print(Fore.GREEN + "Searching Spotify...")
   url = 'https://open.spotify.com/search/'
 
 if site == "yt":
-  print("Searching YouTube...")
+  print(Fore.GREEN + "Searching YouTube...")
   url = 'https://www.youtube.com/results?search_query='
 
 if site == "md":
-  print("Searching Medium...")
+  print(Fore.GREEN + "Searching Medium...")
   url = 'https://www.medium.com/search?q='
 
 if site == "rt":
-  print("Searching Reddit...")
+  print(Fore.GREEN + "Searching Reddit...")
   url = 'https://www.reddit.com/search/?q='
 
 if site == "g":
-  print("Searching Google...")
+  print(Fore.GREEN + "Searching Google...")
   url = 'https://www.google.com/search?q='
+
+if site == "pt":
+  print(Fore.GREEN + "Searching Pinterest...")
+  url = 'https://www.pinterest.com/search/pins?q='
+
+if site == "wk":
+  print(Fore.GREEN + "Searching Wikipedia...")
+  url = 'https://en.wikipedia.org/wiki/'
+
+if site == "x":
+  print(Fore.GREEN + "Searching Twitter...")
+  url = 'https://www.twitter.com/search?q='
 
 def create_query():
   query = sys.argv[2:]
@@ -29,7 +45,7 @@ def create_query():
 
 def create_url():
   if len(sys.argv[2:]) == 0:
-    print("Error: Please Enter a valid search query.")
+    print(Fore.RED + "Error: Please enter a valid search query.")
   else:
     final_url = url + create_query()
     webbrowser.open(final_url)
